b18629f4affe3ee9c5075c8252ca056e4c233459
Rens <rens.dimmendaal@gmail.com>
2025-07-31 16:00:14 +0200

    improve auth howto docs

--- examples/auth/main.py
+++ examples/auth/main.py
@@ -1,40 +1,34 @@
-import os
 from fasthtml.common import *
 from plash_cli.auth import *
 
 app, rt = fast_app()
 
 @rt
-def index(session): 
-    user_id = session.get('user_id')
-    if user_id:
-        return Div(
-            H1("Welcome to My Plash App!"),
-            P(f"You are logged in as user: {user_id}"),
-            A("Logout", href="/logout")
-        )
-    else:
-        return Div(
-            H1("Welcome to My Plash App!"),
-            P("Please sign in to access the app."),
-            A("Sign in with Google", href=mk_plash_signin_url(session))
-        )
+def index(session):
+    if uid:=session.get('uid'):   # <1>
+            return (H1(f"Welcome! You are logged in as user: {uid}"), 
+                    A("Logout", href="/logout"))
+    else: 
+        return (
+            H1("Welcome! Please sign."), 
+            A("Sign in with Google", href=mk_signin_url(session)))  # <2>
 
-@rt(signin_completed_rt)
+@rt(signin_completed_rt)  # <3>
 def signin_completed(session, signin_reply: str):
-    try: uid = goog_id_from_signin_reply(session, signin_reply)
-    except PlashAuthError as e: 
+    try: 
+        uid = goog_id_from_signin_reply(session, signin_reply)  # <4>
+        session['uid'] = uid
+        return RedirectResponse('/', status_code=303)
+    except PlashAuthError as e:  # <5> 
         return Div(
             H2("Login Failed"),
             P(f"There was an error signing you in: {e}"),
             A("Try Again", href="/")
         )
-    session['user_id'] = uid
-    return RedirectResponse('/', status_code=303)
 
 @rt('/logout')
 def logout(session):
-    session.pop('user_id', None)
+    session.pop('uid')  # <6>
     return RedirectResponse('/', status_code=303)
 
 serve()
